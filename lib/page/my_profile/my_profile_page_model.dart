86d0179040c3f987213f97a916f65acd57a187ca
seung <dmdgpdi@naver.com>
2023-10-27 16:30:45 +0900

    Feat: 마이 프로필 뷰 모델 구현

---
+++ lib/page/my_profile/my_profile_page_model.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/model/repository/user_repository.dart';
+
+import '../../model/class/user.dart';
+import './../../model/class/badge.dart' as BadgeClass;
+import './../../model/class/tracker.dart';
+
+enum Status {
+  initial,
+  loading,
+  loaded,
+  error,
+}
+
+class MyProfileViewModel with ChangeNotifier {
+  final UserRepository _userRepository;
+  MyProfileViewModel({required UserRepository userRepository})
+      : _userRepository = userRepository;
+
+  late User _user;
+  late Tracker _tracker;
+  late List<BadgeClass.Badge> _badge;
+  Status status = Status.loading;
+
+  User get user => _user;
+  List<int> get tracker => _tracker.tracker;
+  List<BadgeClass.Badge> get badge => _badge;
+
+  void load() async {
+    try {
+      _user = await _userRepository.getRemoteMyProfile();
+      _tracker = await _userRepository.getRemoteTracker("username");
+      _badge = await _userRepository.getRemoteBadge("username");
+      status = Status.loaded;
+      notifyListeners();
+      debugPrint("loding됨!");
+    } catch (e) {
+      // 추후 경고창으로 전환.
+      debugPrint(e.toString());
+    }
+  }
+}
