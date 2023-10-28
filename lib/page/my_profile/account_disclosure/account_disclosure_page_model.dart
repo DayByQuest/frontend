9ae5dd31a18fef528119800fd8cf14ef5de99679
seung <dmdgpdi@naver.com>
2023-10-29 00:08:09 +0900

    Feat: 계정 공개 범위 페이지 구현

---
+++ lib/page/my_profile/account_disclosure/account_disclosure_page_model.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+import '../../../model/repository/user_repository.dart';
+import '../../common/Status.dart';
+
+class AccountDisclosureViewModel with ChangeNotifier {
+  final UserRepository _userRepository;
+
+  AccountDisclosureViewModel({required UserRepository userRepository})
+      : _userRepository = userRepository;
+
+  Status _status = Status.loading;
+  late bool _isPrivate;
+
+  bool get isPrivate => _isPrivate;
+  Status get status => _status;
+
+  void load() async {
+    try {
+      _isPrivate = await _userRepository.getRemoteVisibility();
+      _status = Status.loaded;
+      notifyListeners();
+    } catch (e) {
+      debugPrint(e.toString());
+    }
+  }
+
+  void changeVisibility(bool isVisibility) async {
+    try {
+      _status = Status.loading;
+      await _userRepository.patchRemoteVisibility(isVisibility);
+      _isPrivate = !_isPrivate;
+      _status = Status.loaded;
+      notifyListeners();
+    } catch (e) {
+      debugPrint(e.toString());
+    }
+  }
+}
