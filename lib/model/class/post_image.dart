34644ea3802d32c7843fc7de0b582649e9627e1c
seung <dmdgpdi@naver.com>
2023-11-13 12:39:40 +0900

    Feat: 피드 조회 모델 설정

--- lib/model/class/post_image.dart
+++ lib/model/class/post_image.dart
@@ -1,13 +1,16 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
 class PostImage {
   String imageUrl;
   bool isSwipe;
+  static String? IMAGE_BASE_URL = dotenv.env['IMAGE_BASE_URL'] ?? '';
 
   PostImage({required this.imageUrl, bool? isSwipe})
       : isSwipe = isSwipe ?? false;
 
   factory PostImage.fromJson(Map<String, dynamic> json) {
     return PostImage(
-      imageUrl: json['imageUrl'],
+      imageUrl: '${IMAGE_BASE_URL}${json['imageIdentifier']}',
       isSwipe: json['isSwipe'] ?? false,
     );
   }
