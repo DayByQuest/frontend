eb5c5371b990eeede903440d4c77bfa8034e94d8
seung <dmdgpdi@naver.com>
2023-11-21 12:40:46 +0900

    Feat: 공통 컴포넌트 추가(공용 마진, 상태)

---
+++ lib/page/common/Gap.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class Gap16 extends StatelessWidget {
+  const Gap16({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox(
+      height: 16,
+      width: 16,
+    );
+  }
+}
+
+class Gap8 extends StatelessWidget {
+  const Gap8({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox(
+      height: 8,
+      width: 8,
+    );
+  }
+}
